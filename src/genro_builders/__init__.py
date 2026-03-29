e935220d265c87b959b8931239adfafafde8e770
Giovanni Porcari <giovanni.porcari@gmail.com>
2026-03-29 15:21:37 +0200

    refactor!: remove BagAppBase, export BuilderManager
    BREAKING: BagAppBase is removed. Use autonomous builder (standalone
    or with BuilderManager) instead. All 30 BagAppBase test scenarios
    are covered by test_autonomous_builder.py.

--- src/genro_builders/__init__.py
+++ src/genro_builders/__init__.py
@@ -1,24 +1,24 @@
 # Copyright 2025 Softwell S.r.l. - SPDX-License-Identifier: Apache-2.0
 """genro-builders: Builder system for genro-bag."""
 
-from genro_builders.app import BagAppBase
 from genro_builders.binding import BindingManager
 from genro_builders.builder import BagBuilderBase
 from genro_builders.builder_bag import BuilderBag, BuilderBagNode
 from genro_builders.compiler import BagCompilerBase, compile_handler
 from genro_builders.component_proxy import ComponentProxy
 from genro_builders.component_resolver import ComponentResolver
+from genro_builders.manager import BuilderManager
 from genro_builders.pointer import PointerInfo, is_pointer, parse_pointer
 
 __version__ = "0.7.0"
 
 __all__ = [
-    "BagAppBase",
     "BagBuilderBase",
     "BagCompilerBase",
     "BindingManager",
     "BuilderBag",
     "BuilderBagNode",
+    "BuilderManager",
     "ComponentProxy",
     "ComponentResolver",
     "PointerInfo",
