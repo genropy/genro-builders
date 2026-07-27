5bc8c61d59fc35289e789b1fd8dfbd3c81f2886b
Giovanni Porcari <giovanni.porcari@gmail.com>
2026-07-27 18:42:30 +0200

    chore: version 0.21.1

--- src/genro_builders/__init__.py
+++ src/genro_builders/__init__.py
@@ -20,7 +20,7 @@ one flat Bag, reachable from any node as ``node.data``.
 
 from genro_builders.builder import BuilderBase, SourceBag, SourceBagNode, container
 
-__version__ = "0.21.0"
+__version__ = "0.21.1"
 
 __all__ = [
     "BuilderBase",
